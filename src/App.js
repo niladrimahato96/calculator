5272c1d8aff4c82d31c7b4a27f8571f7d623cd06
Amit Kumar Jha <ajha@aapnainfotech.com>
2018-12-13 15:07:14 +0530

    git test amit

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component{
 
   makeString = str => {
 // NEED TO FIX THE LOGIC
-// TEST .git
+// TEST .git amit
     if(str === "="){
       this.setState({
         total : ""+Math.round(eval(this.state.queryString)*100)/100,
